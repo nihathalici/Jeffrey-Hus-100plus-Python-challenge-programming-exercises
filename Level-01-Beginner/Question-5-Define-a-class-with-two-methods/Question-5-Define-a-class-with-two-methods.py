238fe9aba940a010e755d19a253255936bbab387
Nihat Halici <nihat.halici@gmail.com>
2024-09-10 00:07:37 +0200

    Add files via upload

---
+++ Level-01-Beginner/Question-5-Define-a-class-with-two-methods/Question-5-Define-a-class-with-two-methods.py
@@ -0,0 +1,31 @@
+"""
+Question 5
+
+Level 1
+
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+
+Hints:
+Use __init__ method to construct some parameters
+
+Solution:
+
+"""
+
+class InOutStr():
+    def __init__(self):
+        self.s = ""
+    
+    def getStr(self):
+        self.s = input("Write something: ")
+
+    def printStr(self):
+        print(self.s.upper())
+
+newstrObj = InOutStr()
+newstrObj.getStr()
+newstrObj.printStr()
