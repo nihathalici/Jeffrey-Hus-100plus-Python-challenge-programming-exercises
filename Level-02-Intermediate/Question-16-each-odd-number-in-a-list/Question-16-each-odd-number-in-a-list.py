4ac22d8d45ccfafbbe7aaa8a9b62db13092999e7
Nihat Halici <nihat.halici@gmail.com>
2024-10-13 00:39:05 +0200

    Add files via upload

---
+++ Level-02-Intermediate/Question-16-each-odd-number-in-a-list/Question-16-each-odd-number-in-a-list.py
@@ -0,0 +1,25 @@
+"""
+Question 16
+
+Level 2
+
+Question: Use a list comprehension to square each odd number in a list. 
+The list is input by a sequence of comma-separated numbers. 
+Suppose the following input is supplied to the program: 
+
+1,2,3,4,5,6,7,8,9 
+
+Then, the output should be: 
+
+1,3,5,7,9
+
+Hints: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Solution:
+"""
+
+values = input("Enter a sequence of comma-separated numbers: ")
+
+odd_numbers = [x for x in values.split(",") if int(x)%2 != 0] 
+
+print(",".join(odd_numbers))
