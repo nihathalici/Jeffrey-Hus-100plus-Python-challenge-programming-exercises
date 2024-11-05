1593f6b360376f183292a90aa018e8021ac85e56
Nihat Halici <nihat.halici@gmail.com>
2024-11-05 06:35:07 +0100

    Add files via upload

---
+++ Level-01-Beginner/Question-23-Calculate-square-value-of-a-number/Question-23-Calculate-square-value-of-a-number.py
@@ -0,0 +1,17 @@
+"""
+Question 23
+
+Level 1
+
+Question: Write a function which can calculate square value of a number
+
+Hints: Using the ** operator
+
+Solution:
+"""
+
+def square(num):
+    return num ** 2
+
+print(square(2))
+print(square(3))
