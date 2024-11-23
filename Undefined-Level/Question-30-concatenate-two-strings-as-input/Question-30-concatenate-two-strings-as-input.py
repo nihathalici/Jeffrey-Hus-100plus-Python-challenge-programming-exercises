a2e13f7a1103eaa7e4b6957843219d146c010a09
Nihat Halici <nihat.halici@gmail.com>
2024-11-24 00:39:25 +0100

    Add files via upload

---
+++ Undefined-Level/Question-30-concatenate-two-strings-as-input/Question-30-concatenate-two-strings-as-input.py
@@ -0,0 +1,21 @@
+""" 
+Question 30
+
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+Hints:
+
+Use + to concatenate the strings
+
+Consider the PEP 8 - Style Guide. Naming a class: PrintValue . Naming a method: print_value .
+
+https://peps.python.org/pep-0008/
+
+Solution:
+"""
+
+def print_val(s1, s2):
+    print(s1 + s2)
+
+# Prints out: 34
+print_val("3", "4")
