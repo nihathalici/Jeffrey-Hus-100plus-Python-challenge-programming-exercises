7548fed911dfbb8aaa12a31c22d135590bf8347d
Nihat Halici <nihat.halici@gmail.com>
2024-12-03 18:14:14 +0100

    Update Question-32-Function-to-differentiate-even-and-odd-numbers.py

--- Undefined-Level/Question-32-Function-to-differentiate-even-and-odd-numbers/Question-32-Function-to-differentiate-even-and-odd-numbers.py
+++ Undefined-Level/Question-32-Function-to-differentiate-even-and-odd-numbers/Question-32-Function-to-differentiate-even-and-odd-numbers.py
@@ -1,6 +1,4 @@
 """ 
-Question-32-Function-to-differentiate-even-and-odd-numbers
-
 Define a function that can accept an integer number as input and print the 
 "It is an even number" if the number is even, otherwise print "It is an odd number".
 
