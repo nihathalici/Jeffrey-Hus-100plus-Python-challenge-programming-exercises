c4f4af23ae667744dc7986b321b6619c0bc7667c
Nihat Halici <nihat.halici@gmail.com>
2024-12-26 09:19:04 +0100

    Add files via upload

---
+++ Undefined-Level/Question-36-Generate-a-dict-values-are-square-of-keys-print-the-keys-only/Question-36-Generate-a-dict-values-are-square-of-keys-print-the-keys-only.py
@@ -0,0 +1,44 @@
+""" 
+Question-36-Generate-a-dict-values-are-square-of-keys-print-the-keys-only
+
+Question 36
+
+Define a function which can generate a dictionary where the keys are numbers between 1 and 20
+ (both included) and the values are square of keys. The function should just print the keys only.
+
+Hints:
+
+Use dict[key]=value pattern to put entry into a dictionary. Use ** operator to get power of a number. Use range() for loops. Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
+
+Consider the PEP 8 - Style Guide. Naming a class: PrintValue . Naming a method: print_value .
+
+https://peps.python.org/pep-0008/
+
+Solution:
+"""
+
+def print_squares():
+    d = dict()
+    for i in range(1, 21):
+        d[i] = i ** 2
+    for k in d.keys():
+        print(k)
+
+
+"""
+1. Alternative Solution:
+"""
+
+def print_squares():
+    d = {i: i ** 2 for i in range(1, 21)}
+    for k in d.keys():
+        print(k)
+
+
+"""
+2. Alternative Solution:
+"""
+
+def print_squares():
+    d = {i: i ** 2 for i in range(1, 21)}
+    print(*d.keys(), sep="\n")
