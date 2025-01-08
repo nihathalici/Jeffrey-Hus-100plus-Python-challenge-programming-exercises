7b0eaa6f3d73d57a7591e15f485c7f5161f79581
Nihat Halici <nihat.halici@gmail.com>
2025-01-09 00:32:19 +0100

    Add files via upload

---
+++ Undefined-Level/Question-38-Generate-a-list-and-print-the-first-5-elements/Question-38-Generate-a-list-and-print-the-first-5-elements.py
@@ -0,0 +1,58 @@
+""" 
+Question 38
+
+
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the first 5 elements in the list.
+
+Hints:
+
+Use ** operator to get power of a number. Use range() for loops. Use list.append() to add values into a list. Use [n1:n2] to slice a list
+
+Consider the PEP 8 - Style Guide. Naming a class: PrintValue . Naming a method: print_value .
+
+https://peps.python.org/pep-0008/
+
+Solution:
+"""
+
+def generate_squares():
+    li = list()
+    for i in range(1, 21):
+        li.append(i ** 2)
+    print(li[:5])
+
+generate_squares() 
+
+"""
+1. Alternative Solution
+"""
+
+def generate_squares():
+    squares = []
+    for i in range(1, 21):
+        square = i ** 2
+        squares.append(square)
+    print(square[:5])
+
+generate_squares()
+
+
+"""
+2. Alternative Solution
+"""
+
+def generate_squares():
+    squares = [i ** 2 for i in range(1, 21)]
+    print(squares[:5])
+
+generate_squares()
+
+
+"""
+3. Alternative Solution
+"""
+
+def generate_squares():
+    print([i ** 2 for i in range(1, 21)][:5])
+
+generate_squares()
