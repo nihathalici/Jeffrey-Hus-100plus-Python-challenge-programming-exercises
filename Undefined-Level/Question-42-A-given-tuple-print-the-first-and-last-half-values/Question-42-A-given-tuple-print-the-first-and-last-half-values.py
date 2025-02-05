e0ed92bde9329f7ee77bc2080721aaf1a174ad63
Nihat Halici <nihat.halici@gmail.com>
2025-02-06 00:04:03 +0100

    Add files via upload

---
+++ Undefined-Level/Question-42-A-given-tuple-print-the-first-and-last-half-values/Question-42-A-given-tuple-print-the-first-and-last-half-values.py
@@ -0,0 +1,51 @@
+"""
+Question 42
+
+With a given tuple (1,2,3,4,5,6,7,8,9,10), write a program to print the first half values 
+in one line and the last half values in one line.
+
+Hints:
+
+Use [n1:n2] notation to get a slice from a tuple.
+
+Consider the PEP 8 - Style Guide. Naming a class: PrintValue . Naming a method: print_value .
+
+https://peps.python.org/pep-0008/
+
+Solution:
+"""
+
+tp = (1,2,3,4,5,6,7,8,9,10)
+tp1 = tp[:5]
+tp2 = tp[5:]
+print(tp1)
+print(tp2)
+ 
+"""
+Alternative Solution-1
+"""
+data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mid = len(data) // 2
+print("First half:", data[:mid])
+print("Second half:", data[mid:])
+
+
+"""
+Alternative Solution-2
+"""
+data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mid = len(data) // 2
+print("First half:", *data[:mid])
+print("Second half:", *data[mid:])
+
+
+
+"""
+Alternative Solution-3
+"""
+
+data = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mid = len(data) // 2
+print(*data[:mid])
+print(*data[mid:])
+
