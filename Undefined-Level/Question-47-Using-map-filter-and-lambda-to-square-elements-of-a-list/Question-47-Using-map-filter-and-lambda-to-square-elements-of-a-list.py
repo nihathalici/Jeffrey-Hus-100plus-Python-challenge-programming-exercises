cc95b9cf550ebf6fe489c9656e14f7f94d3093f3
Nihat Halici <nihat.halici@gmail.com>
2025-03-11 02:55:47 +0100

    Add files via upload

---
+++ Undefined-Level/Question-47-Using-map-filter-and-lambda-to-square-elements-of-a-list/Question-47-Using-map-filter-and-lambda-to-square-elements-of-a-list.py
@@ -0,0 +1,37 @@
+"""
+Question 47
+
+Write a program which can map() and filter() to make a list whose elements are square of 
+even number in [1,2,3,4,5,6,7,8,9,10].
+
+Hints Use map() to generate a list. Use filter() to filter elements of a list. Use lambda to define anonymous functions.
+
+Solution:
+"""
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sq_even_num = map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, num))
+print(*sq_even_num, sep = ", ")
+
+"""
+Alternative Solution-1
+"""
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_num = filter(lambda x: x % 2 == 0, num)
+sq_even_num = map(lambda x: x ** 2, even_num)
+result = list(sq_even_num)
+print(result)
+
+"""
+Alternative Solution-2
+"""
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = list(map(lambda x: x ** 2, filter(lambda x: x % 2 == 0, num)))
+
+"""
+Alternative Solution-3
+"""
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = [x ** 2 for x in num if x % 2 == 0]
