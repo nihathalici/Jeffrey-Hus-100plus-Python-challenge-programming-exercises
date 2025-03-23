8cb429bcddfc857befdbaaeda39785558cd035b1
Nihat Halici <nihat.halici@gmail.com>
2025-03-23 11:44:51 +0100

    Add files via upload

---
+++ Undefined-Level/Question-49-Using-map-and-lambda-to-square-numbers-between-1-and-20/Question-49-Using-map-and-lambda-to-square-numbers-between-1-and-20.py
@@ -0,0 +1,26 @@
+"""
+Question 49
+
+Write a program which can map() to make a list whose elements are square 
+of numbers between 1 and 20 (both included).
+
+Hints Use map() to generate a list. Use lambda to define anonymous functions.
+
+Solution:
+"""
+
+sq_num = map(lambda x: x ** 2, range(1, 21))
+print(*sq_num, sep = ", ")
+"""
+Alternative Solution-1
+"""
+
+sq_num = list(map(lambda x: x ** 2, range(1, 21)))
+print(sq_num)
+
+"""
+Alternative Solution-2
+"""
+
+squares = [x**2 for x in range(1, 21)]
+print(squares)
