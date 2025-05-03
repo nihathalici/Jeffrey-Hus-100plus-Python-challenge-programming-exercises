c7ac271912ca7384d5b3b495ba46abb32aeb2184
Nihat Halici <nihat.halici@gmail.com>
2025-05-04 00:30:15 +0200

    Add files via upload

---
+++ Undefined-Level/Question-56-Write-a-function-compute-5-divide-0-catch-the-exceptions/Question-56-Write-a-function-compute-5-divide-0-catch-the-exceptions.py
@@ -0,0 +1,37 @@
+"""
+Question 56
+
+Write a function to compute 5/0 and use try/except to catch the exceptions.
+
+Hints:
+
+Use try/except to catch exceptions.
+
+Solution:
+"""
+
+def throws():
+    return 5 / 0
+
+try:
+    throws()
+except ZeroDivisionError:
+    print("division by zero!")
+except Exception as err:
+    print('Caught an exception')
+finally:
+    print('In finally block for cleanup')
+
+
+"""
+Alternative Solution:
+"""
+
+def divide_by_zero():
+    try:
+        result = 5 / 0
+    except ZeroDivisionError as e:
+        result = str(e)
+    return result
+
+divide_by_zero()
