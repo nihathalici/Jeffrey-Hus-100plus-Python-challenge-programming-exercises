c65db28488445ff7b460d54f00ed116b93d4ca1f
Nihat Halici <nihat.halici@gmail.com>
2025-05-11 00:09:30 +0200

    Add files via upload

---
+++ Undefined-Level/Question-57-Define-a-custom-exception-class-which-takes-a-string-message-as-attribute/Question-57-Define-a-custom-exception-class-which-takes-a-string-message-as-attribute.py
@@ -0,0 +1,50 @@
+"""
+Question 57
+
+Define a custom exception class which takes a string message as attribute.
+
+Hints:
+
+To define a custom exception, we need to define a class inherited from Exception.
+
+Solution:
+"""
+class MyError(Exception):
+    """ My own exception class
+    
+    Attributes:
+        msg -- explanation of the error
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+"""
+Example usage:
+"""
+
+custom_err = MyError("This is a self defined Custom Exception")
+
+try:
+    raise custom_err
+except MyError as e:
+    print(f"Caught an exception: {e.msg}")
+
+
+
+"""
+Alternative Solution:
+"""
+
+class CustomException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+"""
+Example usage:
+"""
+try:
+    raise CustomException("This is a custom exception message.")
+except CustomException as e:
+    print(f"Caught an exception: {e.message}")
