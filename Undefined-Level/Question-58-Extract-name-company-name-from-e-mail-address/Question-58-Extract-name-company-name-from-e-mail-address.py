b4e27c7464aa97aa3ddbb262aba45594cca96fbb
Nihat Halici <nihat.halici@gmail.com>
2025-05-18 07:27:39 +0200

    Add files via upload

---
+++ Undefined-Level/Question-58-Extract-name-company-name-from-e-mail-address/Question-58-Extract-name-company-name-from-e-mail-address.py
@@ -0,0 +1,71 @@
+"""
+Question 58
+
+Assuming that we have some email addresses in the "username@companyname.com" format, please write program to print the user name of a given email address. Both user names and company names are composed of letters only.
+
+Example: If the following email address is given as input to the program:
+
+john@google.com
+
+Then, the output of the program should be:
+
+john
+
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Hints:
+
+Use \w to match letters.
+
+Solution:
+
+"""
+import re
+
+email_address = input("Enter an email address: ")
+pattern = "(\w+)@((\w+\.)+(com))"
+match = re.match(pattern, email_address) 
+print(match.group(1))
+
+
+
+"""
+Alternative Solution-1:
+"""
+
+import re
+
+def extract_username(email):
+    pattern = r"([a-zA-Z]+)@[a-zA-Z]+\.[a-zA-Z]+"
+    match = re.match(pattern, email)
+
+    if match:
+        return match.group(1)
+    else:
+        return "This email address is invalid."
+
+"""
+Example usage:
+"""
+
+email_address = input("Enter an email address: ")
+username = extract_username(email_address)
+print(username)
+
+
+
+"""
+Alternative Solution-2:
+"""
+
+def extract_username(email):
+    username = email.split('@')[0]
+    return username
+
+"""
+Example usage:
+"""
+
+email = input("Enter an email address: ")
+username = extract_username(email)
+print(username)
