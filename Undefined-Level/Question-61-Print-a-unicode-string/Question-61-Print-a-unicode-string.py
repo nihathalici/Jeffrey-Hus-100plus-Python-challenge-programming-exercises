a530375186aea057d24b7c408eb9d628a49cf336
Nihat Halici <nihat.halici@gmail.com>
2025-06-09 09:09:30 +0200

    Add files via upload

---
+++ Undefined-Level/Question-61-Print-a-unicode-string/Question-61-Print-a-unicode-string.py
@@ -0,0 +1,23 @@
+"""
+Question 61
+
+Print a unicode string "hello world".
+
+Hints:
+
+Use u'strings' format to define unicode string.
+
+Solution:
+"""
+
+unicode_str = u"hello world!"
+print(unicode_str)
+
+"""
+ In Python 3, the 'u' prefix is unnecessary for strings because they are Unicode by default. 
+ Alternative Solution:
+"""
+
+unicode_str = "hello world!"
+print(unicode_str)
+
