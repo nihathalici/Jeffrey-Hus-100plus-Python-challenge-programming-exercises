710871858a7e230092b90e8eeb58792bba1d0572
Nihat Halici <nihat.halici@gmail.com>
2025-06-15 12:17:58 +0200

    Add files via upload

---
+++ Undefined-Level/Question-62-Convert-ASCII-string-to-unicode-string/Question-62-Convert-ASCII-string-to-unicode-string.py
@@ -0,0 +1,34 @@
+"""
+Question 62
+
+Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+
+Hints:
+
+Use unicode() function to convert.
+
+Solution:
+"""
+
+# In Python 3, the unicode() function has been removed.
+
+s = input()
+print(s)
+
+"""
+Alternative Solution:
+"""
+
+def ascii_to_utf8(ascii_string):
+    ascii_bytes = ascii_string.encode('ascii')
+    unicode_string = ascii_bytes.decode('utf-8')
+
+    return unicode_string
+
+"""
+Example usage
+"""
+ascii_string = "Hello, World!"
+unicode_string = ascii_to_utf8(ascii_string)
+print(unicode_string)
+
