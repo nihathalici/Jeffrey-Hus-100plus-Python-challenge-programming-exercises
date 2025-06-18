c415b3041b13a5ff6f05daef8e92c7bc4bb2ee84
Nihat Halici <nihat.halici@gmail.com>
2025-06-19 00:50:22 +0200

    Add files via upload

---
+++ Undefined-Level/Question-63-Write-a-comment-source-code-is-in-unicode/Question-63-Write-a-comment-source-code-is-in-unicode.py
@@ -0,0 +1,17 @@
+"""
+Question 63
+
+Write a special comment to indicate a Python source code file is in unicode.
+
+Solution:
+"""
+
+# -*- coding: utf-8 -*-
+
+#----------------------------------------#
+
+"""
+Alternative Solution:
+"""
+
+# coding: utf-8
