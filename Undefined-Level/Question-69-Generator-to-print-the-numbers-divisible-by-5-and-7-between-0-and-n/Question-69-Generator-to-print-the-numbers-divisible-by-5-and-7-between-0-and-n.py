3e5a45453e20c9956ccb2d832903b6cc471ad4b8
Nihat Halici <nihat.halici@gmail.com>
2025-07-24 00:23:57 +0200

    Add files via upload

---
+++ Undefined-Level/Question-69-Generator-to-print-the-numbers-divisible-by-5-and-7-between-0-and-n/Question-69-Generator-to-print-the-numbers-divisible-by-5-and-7-between-0-and-n.py
@@ -0,0 +1,29 @@
+"""
+Question 69
+
+Please write a program using generator to print the numbers which can be divisible by 5 and 7 between 0 and n in comma separated form while n is input by console.
+
+Example: If the following n is given as input to the program:
+
+100
+
+Then, the output of the program should be:
+
+0,35,70
+
+Hints: Use yield to produce the next value in generator.
+
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+Solution:
+"""
+
+def divisible_by_5_and_7(n):
+    for num in range(n + 1):
+        if num % 5 == 0 and num % 7 == 0:
+            yield num
+
+
+n = int(input("Enter a number: "))
+result = list(divisible_by_5_and_7(n))
+print(",".join(map(str, result)))
